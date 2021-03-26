715eb4b4d79f31e9bdb55cd273d6109edf9cea42
YashDRaja <yashdineshraja@gmail.com>
2021-03-26 19:23:32 -0400

    updated index

--- server/index.js
+++ server/index.js
@@ -1,1 +1,20 @@
 const express = require('express');
+const app = express();
+const port = process.env.PORT || 5000;
+const mongoose = require('mongoose');
+const cors = require('cors');
+const db = require('./db')
+
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(cors())
+app.use(express.json())
+
+db.on('error', console.error.bind(console, 'MongoDB connection error:'))
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+})
+
+app.use('/api', movieRouter)
+
+app.listen(port, () => console.log(`Server running on port ${apiPort}`))
