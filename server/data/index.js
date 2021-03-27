2130d8aa6733c3c590c4604c822b3381c771be42
stefan-gv <49450292+stefan-gv@users.noreply.github.com>
2021-03-27 16:12:15 -0400

    Address -> Geocode

--- server/data/index.js
+++ server/data/index.js
@@ -8,41 +8,48 @@ function mapsData(loc, rad, kw, res){
     let pSearch=[];
     let pDetails=[];
 
-    const params = {
-        key: key,
-        location: loc,
-        radius: rad,
-        region: 'ca',
-        keyword: kw
-    };
-
-    client.placesNearby( { params: params } )
-        .then((r) => {
-            (async () => {
-                for(var i=0; i<r.data.results.length;i++){
-                    pSearch.push(r.data.results[i])
-                    let q = await client.placeDetails({params: {key: key, place_id:r.data.results[i].place_id}})
-                    for (var z=0; z<q.data.result.address_components.length; z++){
-                        if (q.data.result.address_components[z].long_name == "Canada"){
-                            pDetails.push({
-                                name: q.data.result.name,
-                                phoneNum: q.data.result.formatted_phone_number,
-                                address: q.data.result.formatted_address,
-                                website: q.data.result.website,
-                                status: q.data.result.business_status
-                            });
-                        }
-                    }
-                    if (i==r.data.results.length-1){
-                        res.send(pDetails);
-                    }
-                }
-            })()
-        
-        })
-        .catch(e => {
-            console.error('Connection error', e.message)
-    })
+    client.geocode({params:{key: key, address: loc}})
+        .then((g)=>{
+            var geoloc = g.data.results[0].geometry.location
+                
+                const params = {
+                    key: key,
+                    location: geoloc,
+                    radius: rad,
+                    keyword: kw
+                };
+            
+                client.placesNearby( { params: params } )
+                    .then((r) => {
+                        (async () => {
+                            for(var i=0; i<r.data.results.length;i++){
+                                pSearch.push(r.data.results[i])
+                                let q = await client.placeDetails({params: {key: key, place_id:r.data.results[i].place_id}})
+                                for (var z=0; z<q.data.result.address_components.length; z++){
+                                    if (q.data.result.address_components[z].long_name == "Canada"){
+                                        pDetails.push({
+                                            name: q.data.result.name,
+                                            phoneNum: q.data.result.formatted_phone_number,
+                                            address: q.data.result.formatted_address,
+                                            website: q.data.result.website,
+                                            status: q.data.result.business_status,
+                                            photos: q.data.result.photos,
+                                            types: q.data.result.types
+                                        });
+                                    }
+                                }
+                                if (i==r.data.results.length-1){
+                                    res.send(pDetails);
+                                    //console.log(pDetails);
+                                }
+                            }
+                        })()
+                    
+                    })
+                    .catch(e => {
+                        console.error('Connection error', e.message)
+                })
+        })  
 }
 module.exports= mapsData;
 
