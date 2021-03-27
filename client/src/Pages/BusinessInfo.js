3f3749c05f4da58bfd665a0f85a8e7ad313ff35b
nikashan02 <nikashan02@gmail.com>
2021-03-27 17:08:29 -0400

    Updated frontend

--- client/src/Pages/BusinessInfo.js
+++ client/src/Pages/BusinessInfo.js
@@ -1,29 +1,104 @@
 import React from 'react';
 import '../App.css';
-import { Input, Layout, Breadcrumb } from 'antd';
+import Cards from './Cards.js'
+import { Input, Layout, Select, Row, Col, Button, Modal, Slider } from 'antd';
+import {
+  SlidersOutlined
+} from '@ant-design/icons';
 
 const { Content, Footer } = Layout
-const { Search } = Input;
-const onSearch = value => console.log(value);
+const { Option } = Select;
+const marks = {
+  0: '0 km',
+  100: '50 km',
+};
 
 class BusinessInfo extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      inputType: 'Agriculture',
+      inputAddress: '',
+      inputKeywords: '',
+      isModalVisible: false
+    }
+  }
+
+  handleTypeChange = value => {
+    this.setState({ inputType: value })
+  }
+
+  handleAddressChange = e => {
+    this.setState({ inputAddress: e.target.value })
+  }
+
+  onSearch = e => {
+    this.setState({ inputKeywords: e.target.value })
+  }
+
+  showModal = () => {
+    this.setState({ isModalVisible: true })
+  };
+
+  handleOk = () => {
+    this.setState({ isModalVisible: false }) 
+  };
+
+  handleCancel = () => {
+    this.setState({ inputAddress: '' })
+    this.setState({ isModalVisible: false })
+  };
+  
+  formatter = (value) => {
+    return `${value / 2} km`;
+  }
+
   render() {
-    return(
+    return (
       <Layout className="site-layout">
         <Content style={{ margin: '16px 16px' }}>
-          <Search
-            placeholder="Search for a business"
-            allowClear
-            enterButton="Search"
-            size="large"
-            onSearch={onSearch}
-          />
-          <Breadcrumb style={{ margin: '16px 0' }}>
-            <Breadcrumb.Item>Businesses</Breadcrumb.Item>
-            <Breadcrumb.Item>The Old Mill</Breadcrumb.Item>
-          </Breadcrumb>
+          <Row>
+            <Col span={9} style={{ margin: '0 10px' }}>
+              <Input
+                placeholder="Search for a business (separate keywords by commas)"
+                allowClear
+                size="large"
+                onChange={this.onSearch}
+              />
+            </Col>
+            <Col span={5} style={{ margin: '0 10px' }}>
+              <Select
+                defaultValue="Agriculture"
+                style={{ width: "100%" }}
+                onChange={this.handleTypeChange}
+                size={'large'}>
+                <Option value="Agriculture">Agriculture</Option>
+                <Option value="Groceries">Groceries</Option>
+                <Option value="Restaurants">Restaurants</Option>
+                <Option value="Greenhouse">Greenhouse</Option>
+              </Select>
+            </Col>
+            <Col span={2.5} style={{ margin: '0 10px' }}>
+              <Button onClick={this.showModal} size={'large'}>
+                <SlidersOutlined />
+                Set Distance
+              </Button>
+              <Modal title="Set Distance" visible={this.state.isModalVisible} onOk={this.handleOk} onCancel={this.handleCancel}>
+                <Input
+                  placeholder="Address"
+                  size="large"
+                  onChange={this.handleAddressChange}
+                />
+                <Slider tipFormatter={this.formatter} marks={marks} defaultValue={0} />
+              </Modal>
+            </Col>
+            <Col span={2} style={{ margin: '0 10px' }}>
+              <Button type='primary' size={'large'}>Search</Button>
+            </Col>
+          </Row>
           <div className="site-layout-background" style={{ padding: 24, minHeight: 360 }}>
-            Business Info
+            <Cards />
           </div>
         </Content>
         <Footer style={{ textAlign: 'center' }}>The CKWE Business Directory</Footer>
---
+++ client/src/Pages/Cards.js
@@ -0,0 +1,194 @@
+import React from 'react';
+import '../App.css';
+import { Row, Col, Card, Carousel, Space } from 'antd';
+import {
+  LinkOutlined
+} from '@ant-design/icons';
+
+class Cards extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+    }
+  }
+
+  render() {
+    return (
+      <div>
+        <Row gutter={16}>
+          <Col style={{ margin: '8px 0' }} span={6}>
+            <Card title="Business 1">
+              <Carousel autoplay>
+                <img alt='' src="https://source.unsplash.com/random/800x600" />
+                <img alt='' src="https://source.unsplash.com/random/800x600" />
+                <img alt='' src="https://source.unsplash.com/random/800x600" />
+              </Carousel>
+              <br></br>
+              <p><strong>Phone Number: </strong>905-432-5342</p>
+              <p><strong>Address: </strong>7432 Place Rd, Windsor ON</p>
+              <p><strong>Status: </strong>In Business</p>
+              <Space><LinkOutlined />www.google.ca</Space>
+            </Card>
+          </Col>
+          <Col style={{ margin: '8px 0' }} span={6}>
+            <Card title="Business 2">
+              <Carousel autoplay>
+                <img alt='' src="https://source.unsplash.com/random/800x600" />
+                <img alt='' src="https://source.unsplash.com/random/800x600" />
+                <img alt='' src="https://source.unsplash.com/random/800x600" />
+              </Carousel>
+              <br></br>
+              <p><strong>Phone Number: </strong>905-432-5342</p>
+              <p><strong>Address: </strong>7432 Place Rd, Windsor ON</p>
+              <p><strong>Status: </strong>In Business</p>
+              <Space><LinkOutlined />www.google.ca</Space>
+            </Card>
+          </Col>
+          <Col style={{ margin: '8px 0' }} span={6}>
+            <Card title="Business 3">
+              <Carousel autoplay>
+                <img alt='' src="https://source.unsplash.com/random/800x600" />
+                <img alt='' src="https://source.unsplash.com/random/800x600" />
+                <img alt='' src="https://source.unsplash.com/random/800x600" />
+              </Carousel>
+              <br></br>
+              <p><strong>Phone Number: </strong>905-432-5342</p>
+              <p><strong>Address: </strong>7432 Place Rd, Windsor ON</p>
+              <p><strong>Status: </strong>In Business</p>
+              <Space><LinkOutlined />www.google.ca</Space>
+            </Card>
+          </Col>
+          <Col style={{ margin: '8px 0' }} span={6}>
+            <Card title="Business 4">
+              <Carousel autoplay>
+                <img alt='' src="https://source.unsplash.com/random/800x600" />
+                <img alt='' src="https://source.unsplash.com/random/800x600" />
+                <img alt='' src="https://source.unsplash.com/random/800x600" />
+              </Carousel>
+              <br></br>
+              <p><strong>Phone Number: </strong>905-432-5342</p>
+              <p><strong>Address: </strong>7432 Place Rd, Windsor ON</p>
+              <p><strong>Status: </strong>In Business</p>
+              <Space><LinkOutlined />www.google.ca</Space>
+            </Card>
+          </Col>
+          <Col style={{ margin: '8px 0' }} span={6}>
+            <Card title="Business 1">
+              <Carousel autoplay>
+                <img alt='' src="https://source.unsplash.com/random/800x600" />
+                <img alt='' src="https://source.unsplash.com/random/800x600" />
+                <img alt='' src="https://source.unsplash.com/random/800x600" />
+              </Carousel>
+              <br></br>
+              <p><strong>Phone Number: </strong>905-432-5342</p>
+              <p><strong>Address: </strong>7432 Place Rd, Windsor ON</p>
+              <p><strong>Status: </strong>In Business</p>
+              <Space><LinkOutlined />www.google.ca</Space>
+            </Card>
+          </Col>
+          <Col style={{ margin: '8px 0' }} span={6}>
+            <Card title="Business 2">
+              <Carousel autoplay>
+                <img alt='' src="https://source.unsplash.com/random/800x600" />
+                <img alt='' src="https://source.unsplash.com/random/800x600" />
+                <img alt='' src="https://source.unsplash.com/random/800x600" />
+              </Carousel>
+              <br></br>
+              <p><strong>Phone Number: </strong>905-432-5342</p>
+              <p><strong>Address: </strong>7432 Place Rd, Windsor ON</p>
+              <p><strong>Status: </strong>In Business</p>
+              <Space><LinkOutlined />www.google.ca</Space>
+            </Card>
+          </Col>
+          <Col style={{ margin: '8px 0' }} span={6}>
+            <Card title="Business 3">
+              <Carousel autoplay>
+                <img alt='' src="https://source.unsplash.com/random/800x600" />
+                <img alt='' src="https://source.unsplash.com/random/800x600" />
+                <img alt='' src="https://source.unsplash.com/random/800x600" />
+              </Carousel>
+              <br></br>
+              <p><strong>Phone Number: </strong>905-432-5342</p>
+              <p><strong>Address: </strong>7432 Place Rd, Windsor ON</p>
+              <p><strong>Status: </strong>In Business</p>
+              <Space><LinkOutlined />www.google.ca</Space>
+            </Card>
+          </Col>
+          <Col style={{ margin: '8px 0' }} span={6}>
+            <Card title="Business 4">
+              <Carousel autoplay>
+                <img alt='' src="https://source.unsplash.com/random/800x600" />
+                <img alt='' src="https://source.unsplash.com/random/800x600" />
+                <img alt='' src="https://source.unsplash.com/random/800x600" />
+              </Carousel>
+              <br></br>
+              <p><strong>Phone Number: </strong>905-432-5342</p>
+              <p><strong>Address: </strong>7432 Place Rd, Windsor ON</p>
+              <p><strong>Status: </strong>In Business</p>
+              <Space><LinkOutlined />www.google.ca</Space>
+            </Card>
+          </Col>
+          <Col style={{ margin: '8px 0' }} span={6}>
+            <Card title="Business 1">
+              <Carousel autoplay>
+                <img alt='' src="https://source.unsplash.com/random/800x600" />
+                <img alt='' src="https://source.unsplash.com/random/800x600" />
+                <img alt='' src="https://source.unsplash.com/random/800x600" />
+              </Carousel>
+              <br></br>
+              <p><strong>Phone Number: </strong>905-432-5342</p>
+              <p><strong>Address: </strong>7432 Place Rd, Windsor ON</p>
+              <p><strong>Status: </strong>In Business</p>
+              <Space><LinkOutlined />www.google.ca</Space>
+            </Card>
+          </Col>
+          <Col style={{ margin: '8px 0' }} span={6}>
+            <Card title="Business 2">
+              <Carousel autoplay>
+                <img alt='' src="https://source.unsplash.com/random/800x600" />
+                <img alt='' src="https://source.unsplash.com/random/800x600" />
+                <img alt='' src="https://source.unsplash.com/random/800x600" />
+              </Carousel>
+              <br></br>
+              <p><strong>Phone Number: </strong>905-432-5342</p>
+              <p><strong>Address: </strong>7432 Place Rd, Windsor ON</p>
+              <p><strong>Status: </strong>In Business</p>
+              <Space><LinkOutlined />www.google.ca</Space>
+            </Card>
+          </Col>
+          <Col style={{ margin: '8px 0' }} span={6}>
+            <Card title="Business 3">
+              <Carousel autoplay>
+                <img alt='' src="https://source.unsplash.com/random/800x600" />
+                <img alt='' src="https://source.unsplash.com/random/800x600" />
+                <img alt='' src="https://source.unsplash.com/random/800x600" />
+              </Carousel>
+              <br></br>
+              <p><strong>Phone Number: </strong>905-432-5342</p>
+              <p><strong>Address: </strong>7432 Place Rd, Windsor ON</p>
+              <p><strong>Status: </strong>In Business</p>
+              <Space><LinkOutlined />www.google.ca</Space>
+            </Card>
+          </Col>
+          <Col style={{ margin: '8px 0' }} span={6}>
+            <Card title="Business 4">
+              <Carousel autoplay>
+                <img alt='' src="https://source.unsplash.com/random/800x600" />
+                <img alt='' src="https://source.unsplash.com/random/800x600" />
+                <img alt='' src="https://source.unsplash.com/random/800x600" />
+              </Carousel>
+              <br></br>
+              <p><strong>Phone Number: </strong>905-432-5342</p>
+              <p><strong>Address: </strong>7432 Place Rd, Windsor ON</p>
+              <p><strong>Status: </strong>In Business</p>
+              <Space><LinkOutlined />www.google.ca</Space>
+            </Card>
+          </Col>
+        </Row>
+      </div>
+    )
+  }
+}
+
+export default Cards;
