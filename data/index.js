2a8d95cc096b73efa7198486427930dc1c5b137b
nikashan02 <nikashan02@gmail.com>
2021-03-27 00:16:59 -0400

    Updated Data

--- data/index.js
+++ data/index.js
@@ -2,7 +2,7 @@ const {Client} = require("@googlemaps/google-maps-services-js");
 
 const client = new Client( {} );
 
-const key = 'blank';
+const key = 'AIzaSyBtZrvAEwTvdbpiIh13Tz0ctTPhOEwgho0';
 
 let pSearch=[];
 let pDetails=[];
@@ -16,27 +16,21 @@ const params = {
 
 client.placesNearby( { params: params } )
     .then((r) => {
-            (async () => {
-              //  console.log(r.data.results.length)
-                for(var i=0; i<r.data.results.length;i++){
-                    pSearch.push(r.data.results[i])
-                    console.log(i)
-                    client.placeDetails({params: {key: key, place_id:r.data.results[i].place_id}})
-                        .then((q) => {
-            //                console.log(q.data.result)
-                            pDetails.push(q.data.result)
-                            if (i==r.data.results.length-5){
-                                console.log(pSearch)
-                                console.log(pDetails)
-                            }
-                        })   
+        (async () => {
+            for(var i=0; i<r.data.results.length;i++){
+                pSearch.push(r.data.results[i])
+                console.log(i)
+                let q = await client.placeDetails({params: {key: key, place_id:r.data.results[i].place_id}})
+                pDetails.push(q.data.result);
+                if (i==r.data.results.length-1){
+                    console.log(pSearch)
+                    console.log(pDetails)
                 }
-            })()
-
-    }).catch(e => {
+            }
+        })()
+    
+    })
+    .catch(e => {
         console.error('Connection error', e.message)
     })
-setTimeout(() => {
-    console.log(pDetails)
-}, 3000);
 
