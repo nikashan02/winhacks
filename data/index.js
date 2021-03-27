b2d6850bca6d6189e3d0866e6fdd9c1e447a8a15
stefan-gv <49450292+stefan-gv@users.noreply.github.com>
2021-03-27 01:25:47 -0400

    Data Cleanup

--- data/index.js
+++ data/index.js
@@ -1,36 +1,43 @@
-const {Client} = require("@googlemaps/google-maps-services-js");
+function mapsData(loc, rad, kw){
+    const {Client} = require("@googlemaps/google-maps-services-js");
 
-const client = new Client( {} );
+    const client = new Client( {} );
 
-const key = '';
+    const key = 'lol';
 
-let pSearch=[];
-let pDetails=[];
+    let pSearch=[];
+    let pDetails=[];
 
-const params = {
-    key: key,
-    location: { lat: 42.226, lng: -82.546 },
-    radius: 50000,
-    keyword: 'greenhouse'
-};
+    const params = {
+        key: key,
+        location: loc,
+        radius: rad,
+        keyword: kw
+    };
 
-client.placesNearby( { params: params } )
-    .then((r) => {
-        (async () => {
-            for(var i=0; i<r.data.results.length;i++){
-                pSearch.push(r.data.results[i])
-                console.log(i)
-                let q = await client.placeDetails({params: {key: key, place_id:r.data.results[i].place_id}})
-                pDetails.push(q.data.result);
-                if (i==r.data.results.length-1){
-                    console.log(pSearch)
-                    console.log(pDetails)
+    client.placesNearby( { params: params } )
+        .then((r) => {
+            (async () => {
+                for(var i=0; i<r.data.results.length;i++){
+                    pSearch.push(r.data.results[i])
+                    let q = await client.placeDetails({params: {key: key, place_id:r.data.results[i].place_id}})
+                    pDetails.push({
+                        name: q.data.result.name,
+                        phoneNum: q.data.result.formatted_phone_number,
+                        address: q.data.result.formatted_address,
+                        website: q.data.result.website,
+                        status: q.data.result.business_status
+                    });
+                    if (i==r.data.results.length-1){
+                        console.log(pDetails)
+                    }
                 }
-            }
-        })()
-    
+            })()
+        
+        })
+        .catch(e => {
+            console.error('Connection error', e.message)
     })
-    .catch(e => {
-        console.error('Connection error', e.message)
-    })
+    return pDetails
+}
 
